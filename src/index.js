d019f2bb8e73af7aaee751c57523386152496481
Василий <vabednovcska@gmail.com>
2024-09-11 17:31:46 +0500

    35 урок

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from "react";
 import reportWebVitals from "./reportWebVitals";
-import { rerenderEntireTree } from "./render";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { addPost, subscribe, updateNewPostText } from "./redux/state";
+import { BrowserRouter } from "react-router-dom";
 import state from "./redux/state";
 
+let rerenderEntireTree = (state) => {
+  const container = document.getElementById("root");
+  const root = createRoot(container);
+
+  root.render(
+    <BrowserRouter>
+      <App
+        state={state}
+        addPost={addPost}
+        updateNewPostText={updateNewPostText}
+      />
+    </BrowserRouter>
+  );
+};
+
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from "../render";
+let rerenderEntireTree = () => {
+  console.log("state is changed");
+};
 
 let state = {
   profilePage: {
@@ -26,7 +28,7 @@ let state = {
   },
 };
 
-export let addPost = () => {
+export const addPost = () => {
   let newPost = {
     id: 5,
     message: state.profilePage.newPostText,
@@ -38,9 +40,13 @@ export let addPost = () => {
   rerenderEntireTree(state);
 };
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
   state.profilePage.newPostText = newText;
   rerenderEntireTree(state);
+};
+
+export const subscribe = (observer) => {
+  rerenderEntireTree = observer;
 };
 
 export default state;
