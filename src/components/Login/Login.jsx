5458f44cd900850a9da1fda25b8b841296a45e4b
Василий <vabednovcska@gmail.com>
2024-11-05 11:37:43 +0500

    75 урок

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+const LoginForm = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <input
+          {...register("login", { required: true })}
+          name="login"
+          placeholder="Login"
+        />
+      </div>
+      <div>
+        <input
+          {...register("password", { required: true })}
+          name="password"
+          placeholder="Password"
+          type="password"
+        />
+      </div>
+      <div>
+        <input type="checkbox" {...register("rememberMe")} name="rememberMe" />
+        remember me
+      </div>
+      <div>
+        <button type="submit"> Login </button>
+      </div>
+    </form>
+  );
+};
+
+const LoginReduxForm = reduxForm({
+  form: "login",
+})(ContactForm);
 
 const Login = (props) => {
-  return <h1> LOGIN </h1>;
+  return (
+    <div>
+      <h1> LOGIN </h1>
+      <LoginReduxForm onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default Login;
